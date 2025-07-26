30a0e2cb395fba1042d3ddcfe28d9f732811f41b
vaibhavgogo <vaibhavsmartkid@gmail.com>
2025-07-26 21:31:18 +0530

    commit

---
+++ Heap/implementation.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+class heap
+{
+public:
+    int arr[100];
+    int size = 0;
+
+    void insert(int i)
+    {
+        size++;
+        int index = size;
+        arr[index] = i;
+    }
+};
+
+int main()
+{
+    cout << "ad";
+    return 0;
+}
